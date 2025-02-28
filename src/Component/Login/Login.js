2de2365dd6aadc7430799ec1ac5fa16d31a1f13b
cutyduc <chuduc2003work@gmail.com>
2025-02-28 22:33:01 +0700

    cutyduc

--- src/Component/Login/Login.js
+++ src/Component/Login/Login.js
@@ -1,6 +1,7 @@
 import "./Login.css";
 import React from "react";
 import { Link } from "react-router-dom";
+import LoginTab from "./LoginTab";
 
 
 const Login = () => {
@@ -10,9 +11,14 @@ const Login = () => {
         <a href="/" className="login-return"> ← </a>
 
       </div>
-    <div className="login-form"> 
-
-    </div>
+            <div className="login-form"> 
+                <div className="login-form1">
+                    <a href="/" className="login-text">Đăng Nhập / Đăng Ký </a>
+                </div>
+                <div className="login-form2">
+                    <LoginTab />
+                </div>
+            </div>
            
     </div>
   );
